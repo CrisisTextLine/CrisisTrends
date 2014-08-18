69f56b2fe86294ec3d1ecde4814175d70369e9cb
Chris Johnson <chris@crisistextline.org>
2014-08-18 09:19:24 -0400

    Fix dump sql file.

--- data/dump.sql
+++ data/dump.sql
@@ -1,5 +1,5 @@
 
-#select dayofweek(timestamp) dow, hour(timestamp) h, count(*) from message where timestamp > DATE_SUB(curdate(), INTERVAL 8 DAY) and timestamp < curdate() group by dow, h;
+#select dayofweek(timestamp) dow, hour(timestamp) h, count(*) from message where timestamp > DATE_SUB(curdate(), INTERVAL 8 DAY) and timestamp < curdate() group by dow, h
 
 #select count(*) from message WHERE timestamp < curdate();
 
