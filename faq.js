e7929cffcea0859d91b895e8ffa4863b169d986e
Chris Johnson <chris@crisistextline.org>
2016-12-09 17:34:38 -0500

    Update 2016-12-09T22:34:37.709Z

---
+++ faq.js
@@ -0,0 +1,58 @@
+'use strict';
+
+window.faq = {
+  "About Crisis Trends Data": {
+    "image": "http://placekitten.com/g/200/200",
+    "questions": {
+      "Where does the data come from?": {
+        "How is the data collected?": "The visualizations you see are powered by data gathered in our conversations with texters. Following the conversation, volunteer crisis counselors (CCs) complete a survey and select which issues the texter mentioned. We gather this data so that we can get a better understanding of the needs of our texters and how we can best prepare crisis counselors to support them.",
+        "How is the data anonymized?": "To us, texter privacy is first and foremost, always! All data is auto-scrubbed for any personally identifiable information that a texter might share, including names, addresses, dates of birth, phone numbers, and email addresses, prior to sharing with researchers. It is also aggregated before we share it on CrisisTrends.org, because we intend to show you just that, trends!",
+        "How can I filter the data?": "We only display data above a certain threshold of volume in order to protect texter privacy. Therefore, you can filter the data by time (year, month) and location (state). We would not, for example, share that 1 texter from Portland, Oregon texted in the last 24 hours about family issues and depression.",
+        "What do the numbers represent on the graphs?": "Similar to Google Trends, we display trends over time data in terms of relative value. So, we don’t display the absolute volume of certain issues, but rather the relative volume of conversations citing that issue in comparison to all conversations. In data-speak, we call this normalization. Seeing how issues shift over time, relative to our texter population, enables us to discover insights like concerns about financial issues spike around prom season, or that, in comparison to all other states, Montana has the highest proportion of conversations containing suicidal thoughts, vs. other states.",
+        "How “real-time” is the data?": "The value of our data lies in the fact that it is gathered in real-time. Just as Google Trends can pick up early signals of the flu through day-by-day variability in Google searches, Crisis Trends has the potential to pick up early signals of how external events impact mental health through day-by-day variability in crisis conversation topics. We share this data back to you on a daily basis and enable you to filter by time so you can explore different patterns."
+      },
+      "How does Crisis Trends data compare to other survey data about mental health?": {
+          "Representativeness.": "Traditional surveys are good because they target national representativeness, meaning they oversample certain populations when they survey to ensure the final sample of data used for analyses is representative of the U.S. population in terms of demographics, age, gender, etc. Given Crisis Text Line data is gathered more naturalistically, it is representative of a texter-in-crisis population, not the U.S. population; however, this dataset is more useful in a number of ways.",
+          "Daily data gathering.": "Unlike other surveys, which are conducted every year or two years, we gather data every second! Literally. Check here. Then check back tomorrow to see how many new text messages were processed in that time! This velocity of data enables us to explore how crisis occurs in real time and how crisis trends evolve over time. For example, we saw a spike in financial concerns around prom season, a decrease in volume over the summer break, and a spike in conversations tagged with LGBTQ immediately following the Nov 8, 2016 election results.",
+          "Reporting bias.": "Unlike traditional surveys, in which individuals are asked to reflect over the last few months and recall their experiences and emotions, we gather data in-the-moment, when someone is in crisis, via his or her natural language and communication style. Our data is therefore less impacted by variable recall and documented under-reporting in surveys.",
+          "Co-occurring issues.": "Unlike traditional surveys that ask about each mental health concern in isolation, we gather data in the context of unique conversations moments. In one conversation, someone is likely to touch on a variety of issues, and our data can demonstrate how those issues are linked together. For example, we can see in which conversations the texter experienced both suicidal ideation and depression, or what % of bullying conversations also mention self-harm.",
+          "Other mental health surveys and datasets:": "<a href=\"http://www.cdc.gov/healthyyouth/data/yrbs/\" target=\"_blank\">http://www.cdc.gov/healthyyouth/data/yrbs/</a>"
+      },
+      "What can / can’t I do with Crisis Trends data?": {
+        "": "Our goal is to share trends in our data, not to tell you why they occur. We aim to empower you to observe these trends and explore the questions they spark.",
+        "You CAN:": "<ul><li>Explore how the issue mix varies across texters from different states.</li><li>Explore how crisis conversations occur in real-time, across the week and different hours of the day.</li><li>Explore trends for a specific issue.</li></ul>",
+        "You CAN’T:": "<ul><li>Infer reason for the trend (but we encourage you to research your hypotheses further!).</li><li>Select too narrow of a slice of data, as that might compromise texter privacy as well as provide a skewed view from a small sample.</li><li>Assume our texter population is representative of the U.S. as a whole. Roughly 75% of our population is below 25 years old. That being said, we’ve exchanged more than 27M text messages (as of Dec 2016, check now!) since we’ve launched, in every area code in the U.S., so the data on texters-in-crisis is certainly rich.</li></ul>"
+      },
+      "Are there data for area codes or counties?": "We currently share data for states, because we have enough volume at a state level to share aggregate trends without risking texter privacy. We will share data at area code and county level when we have sufficient volume to protect texter anonymity.",
+      "Do you have data from other countries?": "We started our service in 2013 in the U.S. Now, we are excited to be working with partners internationally to increase access to free, 24/7 crisis support via text to people around the world. You can learn more about our plans for international expansion at <a href=\"http://www.crisistextline.org/international/\" target=\"_blank\">Crisis Text Line International</a>.",
+      "What’s the difference between Crisis Trends and Enclave data?": {
+        "This is a question": "This is the answer."
+      },
+      "How should I cite this data?": {
+        "This is a question": "This is the answer."
+      }
+    }
+  },
+
+  "Who texts Crisis Text Line?": {
+    "image": "http://placekitten.com/g/199/199",
+    "questions": {
+      "Where do they come from?": {
+        "This is a question": "This is the answer."
+      },
+      "Who are they?": {
+        "This is a question": "This is the answer."
+      },
+      "What issues are they seeking help for?": {
+        "This is a question": "This is the answer."
+      }
+    }
+  },
+
+  "FAQ": {
+    "image": "http://placekitten.com/g/201/201",
+    "questions": {
+      "This is a question": "This is the answer."
+    }
+  }
+};
