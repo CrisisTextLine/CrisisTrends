39b172fa80066c83fc70ce7840a70b31206a9e77
Casker <casey@mysolace.life>
2017-11-26 02:00:38 -0800

    Update wst.js
    Sanitary hostname matching.

--- app/scripts/wst.js
+++ app/scripts/wst.js
@@ -29,7 +29,7 @@
       window.location.hostname === '[::1]' ||
       // 127.0.0.1/8 is considered localhost for IPv4.
       window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        /^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$/
       )
     );
 
