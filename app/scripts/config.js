00af6e74bf351e166675f6b650fed58aee1d50c7
Shayne Martin <shayne.martin91@gmail.com>
2018-03-09 11:17:15 -0500

    Use multiple dashboards and base which one to use on the viewport

---
+++ app/scripts/config.js
@@ -0,0 +1,20 @@
+(function(window) {
+  window.config = {
+    visualizations: {
+      domain: 'https://app.periscopedata.com',
+      dashboards: {
+        desktop: '4dff3c25-3a2d-4466-955c-3f3638b89fb9',
+        mobile: '171d3869-a831-433b-900d-45e53badfd7f',
+      },
+      constants: {
+        vizHeights: {
+          desktop: 3000,
+          mobile: 4100,
+        },
+        viewportWidths: {
+          mobile: 1000,
+        },
+      },
+    },
+  };
+})(window);
--- app/scripts/sections/visualizations.js
+++ app/scripts/sections/visualizations.js
@@ -1,12 +1,52 @@
 (function() {
-  const DASHBOARD_URL = 'https://app.periscopedata.com/shared/4dff3c25-3a2d-4466-955c-3f3638b89fb9?embed=true';
-
   let loaded = false;
 
   window.addEventListener('scroll', loadDashboard);
   window.addEventListener('resize-complete', reflowDashboard);
+  window.addEventListener('message', messageHandler);
 
-  adjustHeight();
+  /**
+   * @param {Event} e Message event object
+   */
+  function messageHandler(e) {
+    const {origin, data} = e;
+    const domain = window.config.visualizations.domain;
+
+    if (origin !== domain || data.event_type !== 'dashboard_resize') {
+      return;
+    }
+
+    adjustHeight(data.dashboard_height);
+  }
+
+  /**
+   * @return {string} size
+   */
+  function getDashboardSizeName() {
+    const viewportWidths = config.visualizations.constants.viewportWidths;
+    const width = window.innerWidth;
+    const size = Object.entries(viewportWidths)
+      .filter(([name, size]) => width < size)   // Remove viewport sizes smaller than current viewport
+      .reduce((currentLargest, currentSize) => {
+        // If the current size is bigger than the largest size seen so far,
+        // then return that one, otherwise return the existing largest seen so far
+        return currentSize[1] > currentLargest[1] ? currentSize : currentLarget;
+      }, ['desktop', width]); // Start with the smallest size to start width
+
+    return size[0];
+  }
+
+  /**
+   * Gets the dahsboard url for a given size
+   * @param {string} size Name of size to get dashboard for
+   * @return {string} dashboardUrl
+   */
+  function getDashboardUrl(size = getDashboardSizeName()) {
+    const domain = window.config.visualizations.domain;
+    const dashboardId = window.config.visualizations.dashboards[size];
+
+    return `${domain}/shared/${dashboardId}?embed=true`;
+  }
 
   /**
    * Load the dashboard if it hasn't been loaded yet.
@@ -15,7 +55,7 @@
    */
   function loadDashboard(reload) {
     if (!loaded || reload === true) {
-      document.getElementById('viz').src = DASHBOARD_URL;
+      document.getElementById('viz').src = getDashboardUrl();
       loaded = true;
     }
   }
@@ -30,14 +70,20 @@
 
   /**
    * Adjust the height of the data viz.
+   * @param {number} height The height of the iframed document
    */
-  function adjustHeight() {
+  function adjustHeight(height = getDefaultVizHeight()) {
     const viz = document.getElementById('viz');
+    viz.style.height = `${height + 15}px`;
+  }
 
-    if (viz.offsetWidth <= 500) {
-      viz.height = 1950;
-    } else {
-      viz.height = 2600;
-    }
+  /**
+   * @return {number} height
+   */
+  function getDefaultVizHeight() {
+    const heights = config.visualizations.constants.vizHeights;
+    const size = getDashboardSizeName();
+
+    return heights[size];
   }
 })();
